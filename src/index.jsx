fee5b34f5d8f4121c50636c9ab30e53c8a5182e4
domsantini <111545540+domsantini@users.noreply.github.com>
2023-09-11 12:37:00 -0700

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+    return(
+        <h1> yew bish</h1>
+    )
+    
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
